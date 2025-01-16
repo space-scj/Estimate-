2266cb42bddfd8554c2897a1521acf35baee4e63
Steven Joyner <Stevencjoyner91@gmail.com>
2025-01-16 13:51:17 +0000

    version 0.2

---
+++ Readme.md
@@ -0,0 +1,17 @@
+-v.02
+
+##############~/ UPDATED FEATURES \~################
+        Rooms now selectable, and drawing is now toglable based on the draw button.  Selected items will now highlight as a light gray to visually show what is selected.
+        
+        
+        $$$$~/ Features to come \~$$$$
+#Work out collision detection on rooms.
+
+#Test line item functionality with list items pulled from SQLite3.
+
+#Figure out how to implement canvas.delete to reflect and record removal of individual room shapes.
+
+#Amend room objects to be collections of walls, and implement logic to detect if they're contained loops so that the rooms can be modified
+
+#Clean up UI and bar functions
+
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ WIDTH = 800
 HEIGHT = 600
 
 rooms = []
-
+selected = []
 
 lf=12
 
@@ -17,7 +17,8 @@ class application(tk.Tk):
 		super().__init__()
 
 class room():
-	def __init__(self,x1,y1,x2,y2,name,length,width,height):
+	def __init__(self,x1,y1,x2,y2,name,length,width,height,color,rect,rectname):
+		self.color = color
 		self.x1 = x1
 		self.y1 = y1
 		self.x2 = x2
@@ -26,30 +27,67 @@ class room():
 		self.length = length
 		self.width = width
 		self.height = height
+		self.rect = rect
+		self.rectname = rectname
+
 
 def get_count():
 	i = 0
 	for rom in range(len(rooms)):
 		i+=1
 	return i
+
+class drawable():
+	def __init__(self,toggle):
+		self.toggle = toggle
+
+	def toggle_draw(self,toggle):
+		print(self.toggle)
+		if self.toggle == True:
+			self.toggle = False
+			return self.toggle
+		elif self.toggle == False:
+			self.toggle = True
+			return self.toggle
 		
+	def get_toggle(self,toggle):
+		print(self.toggle)
+		return toggle
+		
+draw = drawable(False)
+
+
 	
 def drawrect(event):
-	count = get_count()
-	x,y = event.x,event.y
-	x1,y1, = x+(lf*12),y+(lf*12)
-	rm = room(x,y,x1,y1,f"Room {count}",(x1-x)//12,(y1-y)//12,8)
-	name = f"Room {count}"
-	canvas.create_rectangle(x,y,x1,y1,outline="blue")
-	canvas.create_text(x+(6*lf),y+(6*lf),text=name)
-	rooms.append(rm)
-	for rom in rooms:
-		print(rom.name)
+	if draw.get_toggle(draw.toggle) == True:
+		count = get_count()
+		x,y = event.x,event.y
+		x1,y1, = x+(lf*12),y+(lf*12)
+		rm = room(x,y,x1,y1,f"Room {count}",(x1-x)//12,(y1-y)//12,8,"white", [],"")
+		name = f"Room {count}"
+		rm.rect = canvas.create_rectangle(x,y,x1,y1,outline="blue",fill=rm.color)
+		rm.rectname = canvas.create_text(x+(6*lf),y+(6*lf),text=name)
+		rooms.append(rm)
+		for rom in rooms:
+			print(rom.name)
+	else:
+		for rm in rooms:
+			if event.x > rm.x1 and event.x < rm.x2 and event.y > rm.y1 and event.y < rm.y2:
+				if rm not in selected:
+					selected.append(rm)
+					print(rm)
+					canvas.itemconfigure(rm.rect, fill="light grey")
+					
+					
+			else:
+				selected.remove(rm)
+				rm.color = "white"
+				canvas.itemconfigure(rm.rect,fill="white")
+				
+	
+def deleteRoom(event):
+	canvas.delete()
 
-def dragdraw(event):
-	
-	
-	 
 app = application()
 app.title("Estimate+")
 app.grid_rowconfigure(1,weight=1)
@@ -61,11 +99,17 @@ itemsel = tk.Listbox()
 itemsel.grid(column=0,row=1,sticky="nsew")
 canvas = tk.Canvas(background="white")
 canvas.grid(column=1,row=1,sticky="nsew")
-
+contain = tk.Frame()
+contain.grid(column=0,row=2)
+drawbt = tk.Button(text="Draw room")
+drawbt.grid(column=0, row=2)
+delbt= tk.Button(text="delete",command="deleteRoom")
+delbt.grid(column=1,row=2)
+drawbt.bind("<Button-1>",draw.toggle_draw)
 canvas.bind("<Button-1>",drawrect)
-
+canvas.bind("<Return>", deleteRoom)
+canvas.update()
 
 
 
 app.mainloop()
-
